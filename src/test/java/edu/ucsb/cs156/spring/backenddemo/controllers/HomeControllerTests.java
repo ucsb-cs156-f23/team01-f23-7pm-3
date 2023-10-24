5a4e1d9860915b7adec098b8bd4f68050eedca85
Kevin Ren <kevinrenfc@gmail.com>
2023-10-24 11:09:48 -0700

    Modified home controller and controller tests to include team names and repo

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeControllerTests.java
@@ -49,12 +49,14 @@ public class HomeControllerTests {
         String baseUrl = "http://localhost:8080/";
 
         List<String> team = new ArrayList<String>();
-        team.add("Jing P.");
-        team.add("Christian S.");
-        team.add("Guy W.");
-        team.add("Phill C.");
+        team.add("Kevin R.");
+        team.add("Kate U.");
+        team.add("Ivan H.");
+        team.add("Mariana R.");
+        team.add("Mu C.");
+        team.add("Adyah R.");
         resultMap.put("team",team);
-        resultMap.put("repo","https://github.com/ucsb-cs156-f23/STARTER-team01");
+        resultMap.put("repo","https://github.com/ucsb-cs156-f23/team01-f23-7pm-3");
         resultMap.put("api-documentation", baseUrl + "swagger-ui/index.html");
         String expected = mapper.writeValueAsString(resultMap);
         String actual = HomeController.getHomePageObjectJSON("http://localhost:8080/");
